c9396d3cf3152050be679892a7006cf2bf32d0b3
Vinyla <irma.rustemovic@gmail.com>
2021-05-12 00:12:45 +0200

    set data to my local storage

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoItems from './TodoItems';
 
 const TodoList = () => {
   const [input, setInput] = useState('');
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem('myLocalStorage'));
+    if (todos) {
+      setTodos(data);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('myLocalStorage', JSON.stringify(todos));
+  }, [todos]);
 
   const changeHandler = (e) => {
     setInput(e.target.value);
@@ -11,20 +22,15 @@ const TodoList = () => {
 
   const submitTodos = (e) => {
     e.preventDefault();
-    const newTodos = [
-      ...todos,
-      {
-        text: input
-      }
-    ];
+    const newTodos = [...todos, { text: input }];
     setTodos(newTodos);
     setInput('');
   };
-  
-  const removeTodo = text => {
-    const removeArr = [...todos].filter(input => input.text !== text)
-    setTodos(removeArr)
-  }
+
+  const removeTodo = (text) => {
+    const removeArr = [...todos].filter((input) => input.text !== text);
+    setTodos(removeArr);
+  };
 
   return (
     <div>
